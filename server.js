b9f73ed92a77217d97d54c079fbe6ce4abd945fb
apizzleton <ap@bluffsre.com>
2025-03-10 14:09:45 -0400

    Update CORS configuration to allow localhost development ports

--- server.js
+++ server.js
@@ -7,23 +7,29 @@ require('dotenv').config();
 const app = express();
 
 // CORS Configuration
-app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = process.env.ALLOWED_ORIGINS 
-      ? process.env.ALLOWED_ORIGINS.split(',')
-      : ['https://grokpmfrontend.onrender.com', 'http://localhost:3000'];
-    if (!origin || allowedOrigins.includes(origin) || allowedOrigins.includes('*')) {
+const corsOptions = {
+  origin: function(origin, callback) {
+    const allowedOrigins = [
+      'https://grokpmfrontend.onrender.com',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'http://localhost:3003'
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('CORS policy: Origin not allowed'));
+      callback(new Error('Not allowed by CORS'));
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
-}));
-app.options('*', cors());
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // Request Logging Middleware
 app.use((req, res, next) => {
